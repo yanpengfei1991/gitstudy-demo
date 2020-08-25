b1ed23759f991c303bc3c0490ea7de80952965c7
yan_pengfei <839595970@qq.com>
2020-08-25 21:00:12 +0800

    这是我第一次用mac提交

---
+++ src/main/java/com/yan/study/gitstudydemo/GitstudyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yan.study.gitstudydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yan/study/gitstudydemo/GitstudyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.study.gitstudydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
