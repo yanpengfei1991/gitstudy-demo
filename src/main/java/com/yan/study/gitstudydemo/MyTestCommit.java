031ec28177b9141979bc725ee46b694ed8fc573b
yan_pengfei <839595970@qq.com>
2020-08-26 20:38:43 +0800

    创建了feature1分支并提交

--- src/main/java/com/yan/study/gitstudydemo/MyTestCommit.java
+++ src/main/java/com/yan/study/gitstudydemo/MyTestCommit.java
@@ -2,6 +2,7 @@ package com.yan.study.gitstudydemo;
 
 public class MyTestCommit {
     public static void main(String[] args) {
-        System.out.println("我打算开创我的第一个分支");
+
+        System.out.println("我打算开创我的第一个分支,而且创建了feature分支");
     }
 }
