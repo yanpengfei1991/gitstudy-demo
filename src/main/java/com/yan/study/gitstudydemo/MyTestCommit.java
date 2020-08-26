66136e3c72dc18e34621fd0001b8c7e5f0b31fad
yan_pengfei <839595970@qq.com>
2020-08-26 21:00:38 +0800

    stash以后的提交

--- src/main/java/com/yan/study/gitstudydemo/MyTestCommit.java
+++ src/main/java/com/yan/study/gitstudydemo/MyTestCommit.java
@@ -6,5 +6,7 @@ public class MyTestCommit {
         System.out.println("我打算开创我的第一个分支，我在本地的master分支修改了同一行代码会冲突吗");
 
         System.out.println("我打算开创我的第一个分支,而且创建了feature分支");
+
+        System.out.println("我刚又开创了一个bug分支并使用stash把工作区暂存");
     }
 }
